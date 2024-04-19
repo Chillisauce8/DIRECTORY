d0e845711be71c07164450197a3a5bea5a19189b
Denis <neverminden@gmail.com>
2024-04-19 13:58:01 +0400

    Animate intersection added

--- layouts/default.vue
+++ layouts/default.vue
@@ -82,6 +82,88 @@ const groups = [{
 
 const defaultColors = ref(['green', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet'].map(color => ({ label: color, chip: color, click: () => appConfig.ui.primary = color })))
 const colors = computed(() => defaultColors.value.map(color => ({ ...color, active: appConfig.ui.primary === color.label })))
+
+
+
+const intersectingOptions = {
+  //   root: null,
+  threshold: 0.1,
+  //  rootMargin: '0px',
+};
+
+const intersectingCheck = (animateElements: any[]) => {
+  animateElements.forEach((element) => {
+    if (element.isIntersecting) {
+      element.target.classList.add('INTERSECTING');
+      element.target.classList.add('VIEWED');
+
+      // if (element.target.id) {
+      //   this.intersectedElementsStore.setIntersected(element.target.id);
+      // }
+    } else {
+      if (element.target.classList.contains('INTERSECTING')) {
+        element.target.classList.add('VIEWED');
+        element.target.classList.remove('INTERSECTING');
+
+        // if (element.target.id) {
+        //   this.intersectedElementsStore.resetIntersected(element.target.id);
+        // }
+      }
+    }
+  });
+};
+
+let intersectingObserver: IntersectionObserver;
+
+function animateIntersecting() {
+  const isPlatformBrowser = typeof window !== 'undefined';
+
+  // ANIMATED - Check if Intersecting
+  if (isPlatformBrowser) {
+    const animateElements = document.querySelectorAll('.ANIMATE:not(.VIEWED)') // Set intersection
+
+    animateElements.forEach((element) => {
+      intersectingObserver.observe(element);
+    });
+  }
+}
+
+function animateIntersectingDelayed(delay: number, repeatTimes: number) {
+  animateIntersecting();
+
+  if (!repeatTimes) {
+    repeatTimes = 1;
+  }
+
+  for (let i = 1; i <= repeatTimes; ++i) {
+    setTimeout(() => {
+      animateIntersecting();
+    }, i * delay);
+  }
+}
+
+
+onBeforeMount(() => {
+  intersectingObserver = new IntersectionObserver(
+    intersectingCheck,
+    intersectingOptions
+  );
+
+  animateIntersectingDelayed(500, 1);
+
+// this.pageDataLoadedStore.$onAction(({name, after}) => {
+//   after(() => {
+//     if (name === 'set') {
+//       this.animateIntersectingDelayed(300, 5);
+//     }
+//   });
+// });
+});
+
+onBeforeUnmount(() => {
+  intersectingObserver.disconnect();
+});
+
 </script>
 
 <template>
