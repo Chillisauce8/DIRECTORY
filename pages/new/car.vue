ae03fd39e3908b08e8af09fcd8bed7800d550be5
James Baddiley <james@chillisauce.com>
2024-10-09 04:45:48 +0100

    FancyBox Gallery Added

--- pages/new/car.vue
+++ pages/new/car.vue
@@ -3,6 +3,32 @@
 
     <ContentBlock :title="content1.title" :text="content1.text" :images="content1.images" />
     <ContentBlock :title="content2.title" :text="content2.text" :images="content2.images" />
+    <Fancybox
+        :options="{
+            Carousel: {
+                infinite: false
+            }
+        }"
+    >
+        <a data-fancybox="gallery" href="https://lipsum.app/id/60/1600x1200">
+            <img src="https://lipsum.app/id/60/200x150" width="200" height="150" />
+        </a>
+        <a data-fancybox="gallery" href="https://lipsum.app/id/61/1600x1200">
+            <img src="https://lipsum.app/id/61/200x150" width="200" height="150" />
+        </a>
+        <a data-fancybox="gallery" href="https://lipsum.app/id/62/1600x1200">
+            <img src="https://lipsum.app/id/62/200x150" width="200" height="150" />
+        </a>
+        <a data-fancybox="gallery" href="https://lipsum.app/id/63/1600x1200">
+            <img src="https://lipsum.app/id/63/200x150" width="200" height="150" />
+        </a>
+        <a data-fancybox="gallery" href="https://lipsum.app/id/64/1600x1200">
+            <img src="https://lipsum.app/id/64/200x150" width="200" height="150" />
+        </a>
+        <a data-fancybox="gallery" href="https://lipsum.app/id/65/1600x1200">
+            <img src="https://lipsum.app/id/65/200x150" width="200" height="150" />
+        </a>
+    </Fancybox>
 </template>
 
 <script setup lang="ts">
